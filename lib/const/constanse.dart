e824a50c1504a2f2d05ef96693617b5d76818824
@sajjadarghavani <sajjadarghavani@gmail.com>
2023-06-05 20:48:15 +0330

    add constanse

---
+++ lib/const/constanse.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class Constance {
+  static String titleOne = 'گیاهان را بهتر از قبل درک کنیم';
+  static String titleTwo = 'با گیاهان جدید آشنا شو';
+  static String titleThree = 'با یک گل بهار نمیش ود،گل بکارید';
+  static String descriptionOne =
+      'در مورد نگه داری گل و گیاه میتونی اطلاعات کسب کنی';
+  static String descriptionTwo =
+      'رز مشکی یا گل رز دوست داری ؟ اینجا میتونی پیداش کنی';
+  static String descriptionThree =
+      'هرگلی نیاز داشته باشید در این اپلیکیشن پیدا می کنید';
+  static Color myGreen = const Color(0xff102009);
+  static Color myGrey = const Color(0xff8b9192);
+  static Color myGreenLight = const Color(0xff325a20);
+}
