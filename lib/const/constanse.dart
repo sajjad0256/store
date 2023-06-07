c16e6ef002f7387e04640372d2fe9949baf40c6f
@sajjadarghavani <sajjadarghavani@gmail.com>
2023-06-07 14:03:27 +0330

    update

--- lib/const/constanse.dart
+++ lib/const/constanse.dart
@@ -4,16 +4,19 @@ class Constance {
   static String titleAppbar = 'رد کردن';
   static String titleOne = 'گیاهان را بهتر از قبل درک کنیم';
   static String titleTwo = 'با گیاهان جدید آشنا شو';
-  static String titleThree = 'با یک گل بهار نمیش ود،گل بکارید';
+  static String titleThree = 'با یک گل بهار نمیشود،گل بکارید';
   static String descriptionOne =
       'در مورد نگه داری گل و گیاه میتونی اطلاعات کسب کنی';
   static String descriptionTwo =
       'رز مشکی یا گل رز دوست داری ؟ اینجا میتونی پیداش کنی';
   static String descriptionThree =
       'هرگلی نیاز داشته باشید در این اپلیکیشن پیدا می کنید';
-  static Color myGreen = const Color(0xff102009);
+  static Color myGreen = const Color.fromARGB(255, 48, 92, 30);
   static Color myGrey = const Color(0xff8b9192);
-  static Color myGreenLight = const Color(0xff325a20);
+  static Color myGreyLight = const Color.fromARGB(255, 173, 175, 172);
   static Color myWhite = const Color.fromARGB(255, 255, 255, 255);
-  static Color myBlack = Color.fromARGB(255, 20, 20, 20);
+  static Color myPrimary = const Color.fromARGB(255, 79, 92, 111);
+  static Color myBlack = const Color.fromARGB(255, 20, 20, 20);
+  static Color myBlue = const Color.fromARGB(255, 43, 77, 248);
+  static double sizeTitleAppbar = 22;
 }
